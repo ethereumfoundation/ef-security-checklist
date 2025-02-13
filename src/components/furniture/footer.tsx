a91f7b38412d341cd0fd3a3c2bbff9c24c09d8d5
Oleh Aldekein <i.am@aldeke.in>
2025-02-13 19:36:10 +0200

    Public release preparations.

--- src/components/furniture/footer.tsx
+++ src/components/furniture/footer.tsx
@@ -3,14 +3,18 @@ import { component$ } from "@builder.io/qwik";
 export default component$(() => {
 
   const licenseLink = 'https://github.com/Lissy93/personal-security-checklist/tree/master/web';
+  const sourceLink = 'https://github.com/ethereumfoundation/ef-security-checklist';
   const authorLink = 'https://aliciasykes.com';
 
   return (
   <footer class="footer footer-center px-4 py-2 mt-4 text-base-content bg-base-200 bg-opacity-25">
     <aside>
-      <p><a href={licenseLink} class="link link-primary">Original checklist</a> licensed under MIT -
-      © <a href={authorLink} class="link link-primary">Alicia Sykes</a> - 
-      Customized for the EF</p>
+      <p>
+        Based on the <a href={licenseLink} class="link link-primary">original checklist</a> licensed under MIT 
+        by <a href={authorLink} class="link link-primary">Alicia Sykes</a>.<br/>
+
+        Heavily customized for the EF, <a href={sourceLink} class="link link-primary">source code</a> available on GitHub.
+      </p>
     </aside>
   </footer>
   );
